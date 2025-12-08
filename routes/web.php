46eced853af67dca39f4bf16b4569e1453c815ea
Shojoian <bjaynomoney@gmail.com>
2025-12-09 01:05:44 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 | Storefront Routes
 |--------------------------------------------------------------------------
 */
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrated!';
+});
+
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
 
